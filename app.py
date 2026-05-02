2a3ba79731b22eff42d5c61d59fcceb29e2aa08f
jp0431 <74320842+jp0431@users.noreply.github.com>
2026-05-02 16:21:50 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,181 @@
+import streamlit as sl
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import plotly.express as px
+import ipywidgets as widgets
+from IPython.display import display
+import plotly.io as pio
+
+# Funciones 
+def incializa_metricas(data):
+    #Canciones repetidas
+    top_repes = data[data["skipped"]== False]['trackName'].value_counts().reset_index().head(10)
+    #Canciones más escuchadas
+    top_canciones = data.groupby("trackName")['msPlayed'].sum().sort_values(ascending=False).reset_index().head(5)
+    #Artitstas escuchadas
+    top_artistas = data.groupby("artistName")['msPlayed'].sum().sort_values(ascending=False).reset_index().head(5)
+    #Total horas escuchadas
+    horas_escuchadas = data["HoursPlayed"].sum()
+    #total Canciones escuchcadas
+    n_canciones = len(data['trackName'])
+    #total artistas escuchados
+    n_artits = data['artistName'].nunique()
+    #Total canciones skipeadas
+    skips = len(data[data["skipped"] == True])
+    #% canciones skipeadas
+    percen_skips = (skips * 100 /n_canciones)
+    artists_per_month = data.groupby("month_entropy")["artistName"].nunique()
+    avg_artist = artists_per_month.mean()
+    artist_counts = data["artistName"].value_counts(normalize=True)
+    entropy = -np.sum(artist_counts * np.log(artist_counts))
+    score = 0.7 * avg_artist + 0.3 * entropy
+    return top_canciones, top_artistas,top_repes, horas_escuchadas, n_canciones, n_artits, skips, percen_skips, score
+def crear_graficos(top_canciones,top_artistas, top_repes, data):
+    top_canciones['minPlayed'] = top_canciones['msPlayed'] / 60000
+    fig_songs = px.bar(top_canciones,x='trackName',y='minPlayed', title="Top Canciones escuchadas", labels={"trackName": "Canción", "minPlayed":"Minutos"})
+    
+    top_artistas['minPlayed'] = top_artistas['msPlayed'] / 60000
+    fig_repes = px.bar(top_repes,x='trackName',y='count', title="Top canciones más repetidas", labels={"trackName": "Canción", "count":"Veces Repetida"})
+    horas_escucha=data.groupby("hour")["HoursPlayed"].sum().reset_index()
+    fig_line = px.line(horas_escucha, x='hour', y='HoursPlayed', title="Escucha por horas")
+    fig_artist = px.bar(top_artistas,x='artistName',y='minPlayed', title="Top artitas escuchados", labels={"artistName": "Autor", "minPlayed":"Minutos"})
+    dias= data.groupby("day")["HoursPlayed"].sum().reset_index()
+    heatmap = data.pivot_table(
+        index="day",
+        columns="hour",
+        values="HoursPlayed",
+        aggfunc="sum"
+    ).fillna(0)
+    days_order = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    heatmap = heatmap.reindex(days_order)
+
+
+    fig_heatmap = px.imshow(
+        heatmap,
+        aspect="auto",
+            color_continuous_scale=[   "#0b0f2b",
+        "#3b0f70",
+        "#8c2981",
+        "#de4968",
+        "#fe9f6d",
+        "#fcfdbf"]
+    ,  # 🔥 estilo como tu ejemplo
+        labels=dict(x="Hora del día", y="Día", color="Horas"),
+        template="plotly_dark",
+
+    )
+    fig_heatmap.update_layout(
+        margin=dict(l=20, r=20, t=50, b=20),
+
+    )
+    return fig_songs, fig_artist, fig_repes, fig_line,fig_heatmap
+    #print("Grafico canciones creado")
+# CONFIG
+sl.set_page_config("SPOTIFY DASHBOARD")
+sl.title("SPOTIFY DASHBOARD Q1 2026")
+# Cargar datos
+data1 = pd.read_json("Data/StreamingHistory_music_0.json")
+data2 = pd.read_json("Data/StreamingHistory_music_1.json")
+dfs = [data1, data2] #Convertimos el tipo de datos de la columna endTime
+for df in dfs:
+    df["endTime"] = pd.to_datetime(df["endTime"])
+data1 = data1[data1["endTime"] >= "2026-01-01"] #Seleccionamos los datos a partir de 2026
+data2 = data2[data2["endTime"] < "2026-04-01"]#Seleccionamos los datos hasta abril de 2026
+data = pd.concat([data1, data2])
+#Creamos las columnas de horas Escuchadas, minutos escuchados, la hora, el día y si se ha skipeado la canción
+data["HoursPlayed"] = data["msPlayed"] / (1000*60*60)
+data["MinutesPlayed"] = data["msPlayed"] /  60000
+data["hour"] = data["endTime"].dt.hour
+data["month"] = data["endTime"].dt.month
+data["day"] = data["endTime"].dt.day_name()
+data["skipped"] = data["msPlayed"] < 30000
+data = data.sort_values("endTime")
+data["gap"] = data["endTime"].diff().dt.seconds
+data["month_entropy"] = data["endTime"].dt.to_period("M")
+
+
+
+# Filtros
+sl.sidebar.header("Filtros")
+month = sl.sidebar.selectbox("Selecciona mes", ["Todos"]+sorted(data["month"].unique()))
+artista = sl.sidebar.selectbox("Selecciona el artisa", ["Todos"]+sorted(data["artistName"].unique()))
+# Filtrar
+df = data.copy()
+if month != "Todos":
+    df = df[df["month"] == month]
+if artista != "Todos":
+    df = df[df["artistName"] == artista]
+# Metricas
+top_canciones, top_artistas,top_repes, horas_escuchadas, n_canciones, n_artits, skips, percen_skips, score = incializa_metricas(df)
+sl.write(
+    f"Has escuchado {horas_escuchadas:.0f} horas de música "
+    f"y descubierto {n_artits} artistas diferentes."
+)
+c1, c2,c3,c4 = sl.columns(4)
+c1.metric("🎧 Horas", f"{horas_escuchadas:.1f}")
+c2.metric("🎵 Canciones escuchadas", f"{n_canciones}")
+c3.metric("🎤 Artitstas", f"{n_artits}")
+c4.metric("⏭️ % Skips: ",f"{percen_skips:.2f}")
+
+#Graficos
+
+fig_songs, fig_artist, fig_repes, fig_line,fig_heatmap = crear_graficos(top_canciones,top_artistas, top_repes, df)
+
+sl.markdown("## 🕒 ¿Cuándo escuchas música?")
+
+sl.plotly_chart(fig_heatmap)
+horas = df.groupby("hour")["HoursPlayed"].sum().reset_index()
+horas = horas.sort_values("HoursPlayed", ascending= False)["hour"].iloc[0]
+sl.markdown(f"🎯 Sueles escuchar música a las {int(horas)}:00")
+
+sl.markdown("## 🧠 Tu perfil musical")
+
+# Insights
+text = ""
+if score < 20:
+    text = "Baja"
+elif score  >= 20 and score < 81: 
+    text = "media"
+elif score > 80 and score <151:
+    text = "alta"
+elif score > 150:
+    text = "Muy alta"
+
+col1, col2 = sl.columns(2)
+col1.metric("🔍 Exploración", text)
+col2.metric("🎧 Engagement", f"{100 - percen_skips:.0f}%")
+
+top_artista =  top_artistas.iloc[0]["artistName"]
+top_artista_count = top_artistas.iloc[0]["msPlayed"] / df["msPlayed"].sum() * 100
+if artista == "Todos":
+    sl.markdown("## Artistas")
+    sl.write(f"Mi artista favorito es **{top_artista}**", f"Representando un **{top_artista_count:.2f}%** del tiempo")
+    sl.plotly_chart(fig_artist)
+sl.markdown("## 🎶 Tus canciones favoritas")
+p1, p2,= sl.columns(2)
+with p1:
+    sl.plotly_chart(fig_songs)
+with p2:
+    sl.plotly_chart(fig_repes)
+
+sl.markdown("## ⏭️ Cómo escuchas música")
+
+sl.write(
+    f"Saltas aproximadamente el **{percen_skips:.1f}%** de las canciones, "
+    f"lo que indica un comportamiento "
+    f"{'explorador' if percen_skips > 30 else 'selectivo'}."
+)
+sl.markdown("## 🎯 Conclusión")
+horario = ""
+if horas > 6 and horas <=11:  horario = "la mañana"
+elif horas > 11 and horas < 14: horario ="el mediodia"
+elif horas >=14 and horas < 20: horario = "la tarde"
+elif horas >= 20 and horas < 2: horario = "la noche"
+elif horas >= 2 and horas <= 6: horario = "la madrugada" 
+
+sl.write(
+    f"Eres un oyente con un perfil **{text}**, "
+    f"que escucha principalmente por **{horario}** y "
+    f"explora una gran variedad de artistas."
+)
